90ae7f3836990adc61a06a3c6658758f8502641f
Tommy Lockwood <t.lockwood@erasolutions.us>
2026-07-01 14:14:25 -0500

    Added about and history routes to demo routing

---
+++ src/app/about/layout.jsx
@@ -0,0 +1,8 @@
+export default function AboutLayout({ children }) {
+  return (
+    <>
+      <header className="bg-purple-300 p-2 m-2 font-medium">About section header</header>
+      {children}
+    </>
+  );
+}
